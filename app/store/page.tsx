f2dda4472e543951e81da6baebad8e04f1ea76bf
Hossein <141348426+SHEKAF1234@users.noreply.github.com>
2025-03-02 11:26:40 +0330

    store and detailpage

--- app/store/page.tsx
+++ app/store/page.tsx
@@ -1,8 +1,9 @@
 "use client";
 import Image from "next/image";
+import Link from "next/link";
 import { FC } from "react";
 import { motion } from "framer-motion";
-import { FiShoppingCart, FiHeart } from "react-icons/fi";
+import { FiHeart, FiEye } from "react-icons/fi";
 
 interface ProductProps {
   id: string;
@@ -16,7 +17,7 @@ interface ProductListProps {
   products: ProductProps[];
 }
 
-const products = [
+const products: ProductProps[] = [
   {
     id: "1",
     title: "محصول 1",
@@ -35,7 +36,7 @@ const products = [
   },
   {
     id: "3",
-    title: "محصول 2",
+    title: "محصول 3",
     description:
       "در کادر زیر هر متنی را که دوست دارید تایپ کنید تا ما آن را برایتان نگه داریم و همیشه در دسترس شما قرار دهیم؛ ",
     price: 100000,
@@ -46,13 +47,13 @@ const products = [
 const Store: FC<ProductListProps> = () => {
   return (
     <div
-      className=" px-4 py-8 bg-gradient-to-l from-[#16222A] to-[#3A6073]"
+      className="px-4 py-8 bg-gradient-to-l from-[#16222A] to-[#3A6073]"
       dir="rtl"
     >
-      <h2 className="text-4xl font-bold text-center text-yellow-500 mb-8">
+      <h2 className="text-4xl mt-24 font-bold text-center text-yellow-500 mb-8">
         محصولات ما
       </h2>
-      <div className="grid mt-24 grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
         {products.map((product) => (
           <motion.div
             key={product.id}
@@ -88,29 +89,28 @@ const Store: FC<ProductListProps> = () => {
               <h3 className="text-xl font-bold text-gray-50 mb-2 line-clamp-1">
                 {product.title}
               </h3>
-
               <p className="text-gray-200 text-sm mb-4 line-clamp-2">
                 {product.description.slice(0, 50)}...
               </p>
-
               <div className="flex items-center justify-between">
                 <div className="space-y-1">
                   <p className="text-xs text-gray-300">قیمت محصول</p>
-                  <span className="text-base font-bold text-yellow-600">
+                  <span className="text-sm font-bold text-yellow-600">
                     {product.price} تومان
                   </span>
                 </div>
-
-                <motion.button
-                  whileTap={{ scale: 0.95 }}
-                  aria-label="Add to cart"
-                  className="flex items-center gap-2 bg-yellow-500 hover:bg-yellow-600 text-white px-6 py-3 rounded-xl font-medium transition-colors duration-200"
-                >
-                  <FiShoppingCart className="w-5 h-5" />
-                  <span className="text-sm text-nowrap font-semibold">
-                    افزودن به سبد خرید
-                  </span>
-                </motion.button>
+                <Link href={`/store/${product.id}`}>
+                  <motion.button
+                    whileTap={{ scale: 0.95 }}
+                    aria-label="View product"
+                    className="flex items-center gap-2 bg-yellow-500 hover:bg-yellow-600 text-white px-6 py-3 rounded-xl font-medium transition-colors duration-200"
+                  >
+                    <span className="text-xs text-nowrap font-semibold">
+                      مشاهده محصول
+                    </span>
+                    <FiEye className="w-4 h-4" />
+                  </motion.button>
+                </Link>
               </div>
             </div>
           </motion.div>
