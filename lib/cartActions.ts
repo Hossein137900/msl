d7364175a0d5fead52196bacac58d2e5426bb1fd
Hossein <141348426+SHEKAF1234@users.noreply.github.com>
2025-03-04 14:01:58 +0330

    Update cartActions.ts

--- lib/cartActions.ts
+++ lib/cartActions.ts
@@ -1,56 +1,55 @@
-'use server';
+"use server";
 import { prisma } from "./prisma";
-import * as jwt from 'jsonwebtoken';
+import * as jwt from "jsonwebtoken";
 
 export async function addToCart(formData: FormData, token: string) {
-    const secret = process.env.JWT_SECRET;
-    if (!secret) {
-        console.log('JWT_SECRET is not defined in environment variables');
+  const secret = process.env.JWT_SECRET;
+  if (!secret) {
+    console.log("JWT_SECRET is not defined in environment variables");
+  }
+
+  try {
+    const decodedToken = (await jwt.verify(token, "msl")) as { userId: string };
+    const userId = decodedToken?.userId;
+
+    if (!userId) {
+      throw new Error("User ID not found in token");
     }
 
-    try {
-        const decodedToken = await jwt.verify(token, 'msl') as { userId: string };
-        const userId = decodedToken?.userId;
-        
-        if (!userId) {
-            throw new Error('User ID not found in token');
-        }
+    const productId = formData.get("productId") as string;
+    const quantity = parseInt(formData.get("quantity") as string);
+    const path = formData.get("path") as string;
+    const image = formData.get("image") as string;
+    const totalPrice = parseInt(formData.get("totalPrice") as string);
 
-        const productId = formData.get('productId') as string;
-        const quantity = parseInt(formData.get('quantity') as string);
-        const path = formData.get('path') as string;
-        const image = formData.get('image') as string;
-        const totalPrice = parseInt(formData.get('totalPrice') as string);
+    // First, find or create a cart for the user
+    let cart = await prisma.cart.findFirst({
+      where: { userId },
+    });
 
-        // First, find or create a cart for the user
-        let cart = await prisma.cart.findFirst({
-            where: { userId }
-        });
-
-        if (!cart) {
-            cart = await prisma.cart.create({
-                data: {
-                    userId,
-                    path,
-                    image,
-                    totalPrice,
-                }
-            });
-        }
-
-        // Create a cart item and associate it with the cart
-        const cartItem = await prisma.cartItem.create({
-            data: {
-                cartId: cart.id,
-                productId,
-                quantity,
-            }
-        });
-
-        return cartItem;
+    if (!cart) {
+      cart = await prisma.cart.create({
+        data: {
+          userId,
+          path,
+          image,
+          totalPrice,
+        },
+      });
     }
-    catch (error) {
-        console.error(error);
-        throw error;
-    }
+
+    // Create a cart item and associate it with the cart
+    const cartItem = await prisma.cartItem.create({
+      data: {
+        cartId: cart.id,
+        productId,
+        quantity,
+      },
+    });
+
+    return cartItem;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
 }
